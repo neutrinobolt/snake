8b6e9e28148b4157cb5e1b5cde947d5e723098f6
neutrinobolt <bescsn@gmail.com>
2024-04-10 22:38:54 -0700

    Create board

--- board.py
+++ board.py
@@ -11,6 +11,10 @@ Board has:
 
 import tkinter as tk
 
+#Constants
+TIME_STEP = .1
+GRID_STEP = 30
+
 class Board:
     def __init__(self, 
                  hscore_current):
@@ -18,11 +22,29 @@ class Board:
         #Define preset values
         self.hscore = hscore_current
 
-        #Define constants
-        TIME_STEP = .1
-        GRID_STEP = 10
+        
+        
 
-        #
+        #Iteration specific stuff
+        self.curr_score = 0
+        self.tails = []
+
+        #Gui setup
+        self.window = tk.Tk()
+        self.window.title("Snake")
+        self.canvas = tk.Canvas(self.window, width=800, height=600, bg="black")
+        self.canvas.pack()
+        #Setup appearance
+        self.canvas.create_rectangle(25, 25, 775, 525, fill='black', outline='white')
+        self.canvas.create_text(400, 540,
+                                text = f'Current score: {self.curr_score}',
+                                fill= 'white',
+                                font=('Helvetica 10 bold'))
+        self.canvas.create_text(400, 560,
+                                text = f'High score: {self.hscore}',
+                                fill= 'white',
+                                font=('Helvetica 10 bold'))
+
 
     def apple(self):
         pass
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import board
 def main ():
     """Main running function."""
     #Get previous high score
-    with open("hscore.txt", "r", encoding= "UTF-8") as infile:
+    with open('snake\hscore.txt', "r", encoding= "UTF-8") as infile:
         hi_score = infile.read()
     #Create board
     play_board = board.Board(hi_score)
     #Run board
+    play_board.window.mainloop()
 
 if __name__ == "__main__":
     main()
