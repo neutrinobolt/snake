72a1c2580e02ad5c313ae291c5432f4637ff7983
neutrinobolt <bescsn@gmail.com>
2025-03-22 15:47:19 -0700

    small syntax fix

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import board
 def main ():
     """Main running function."""
     #Get previous high score
-    with open('snake\hscore.txt', "r", encoding= "UTF-8") as infile:
+    with open('hscore.txt', "r", encoding= "UTF-8") as infile:
         hi_score = infile.read()
-        
+
     #Create board and run board
     play_board = board.Board(hi_score)
     play_board.window.mainloop()
